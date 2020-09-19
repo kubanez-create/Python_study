230031ef4ed5cdb7c990baef5452be73e4e72e73
kubanez-create <57756022+kubanez-create@users.noreply.github.com>
2020-09-19 22:38:31 +0300

    Add files via upload

--- storage.py
+++ storage.py
@@ -2,19 +2,52 @@
 import os
 import tempfile
 import argparse
+import json
 
-parser = argparse.ArgumentParser(description="store the data in storage")
-parser.add_argument("--key", type=str, help="the key")
-parser.add_argument("--value", type=int, help="the value")
-args = parser.parse_args()
+def my_func():
+    parser = argparse.ArgumentParser(description="store the data in storage")
+    parser.add_argument("--key", type=str, help="the key")
+    parser.add_argument("--val", type=int, help="the value")
+    args = parser.parse_args()
 
-if args.key and args.value:
-    d = {args.key: args.value}
-if args.key in d.keys:
-    d[args.key].append({args.key: args.value})
+    if os.path.exists('storage.data'):
+        d = {}
+        if args.key and args.val:
+            if args.key not in d.keys():
+                d[args.key] = args.val
+            elif args.key in d.keys():
+                if type(d[args.key]) == type([]):
+                    d[args.key].append(args.val)
+                elif type(d[args.key]) != type([]):
+                    tv = d[args.key]
+                    d[args.key] = []
+                    d[args.key].append(tv)
+        with open('storage.data', 'w') as f:
+            json.dump(d, f)
+    
+    d = {}
 
-storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
-with open(storage_path, 'w') as f:
+    if args.key and args.val:
+        if args.key not in d.keys():
+            d[args.key] = args.val
+        elif args.key in d.keys():
+            if type(d[args.key]) == type([]):
+                d[args.key].append(args.val)
+            elif type(d[args.key]) != type([]):
+                tv = d[args.key]
+                d[args.key] = []
+                d[args.key].append(tv)
+            
 
+    storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
 
-answer = args.x**args.y
+    with open(storage_path, 'w') as f:
+        json.dump(d, f)
+
+    if args.key and not args.val:
+        with open(storage_path) as json_file:
+            data = json.load(json_file)
+            print(data)
+
+if __name__ == "__main__":
+    my_func()
