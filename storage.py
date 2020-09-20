409b2e903beb5312f30019140e5bcf9253e98800
kubanez-create <57756022+kubanez-create@users.noreply.github.com>
2020-09-20 14:57:37 +0300

    Add files via upload

--- storage.py
+++ storage.py
@@ -7,51 +7,41 @@ import json
 def my_func():
     parser = argparse.ArgumentParser(description="store the data in storage")
     parser.add_argument("--key", type=str, help="the key")
-    parser.add_argument("--val", type=int, help="the value")
+    parser.add_argument("--val", help="the value")
     args = parser.parse_args()
 
-    if not os.path.exists('storage.data'):
+    if not os.path.exists(os.path.join(tempfile.gettempdir(), 'storage.data')):
         d = {}
         if args.key and args.val:
-            if args.key not in d.keys():
-                d[args.key] = args.val
-            elif args.key in d.keys():
-                if type(d[args.key]) == type([]):
-                    d[args.key].append(args.val)
-                elif type(d[args.key]) != type([]):
-                    tv = d[args.key]
-                    d[args.key] = []
-                    d[args.key].append(tv)
-        storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
+            d[args.key] = []
+            d[args.key].append(args.val)
+            storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
 
-        with open(storage_path, 'w') as f:
-            json.dump(d, f)
+            with open(storage_path, 'w') as f:
+                json.dump(d, f)
         
     
     else:
-        d = {}
+        with open(os.path.join(tempfile.gettempdir(), 'storage.data')) as feed:
+            d = json.load(feed)
 
         if args.key and args.val:
             if args.key not in d.keys():
-                d[args.key] = args.val
+                d[args.key] = []
+                d[args.key].append(args.val)
             elif args.key in d.keys():
-                if type(d[args.key]) == type([]):
-                    d[args.key].append(args.val)
-                elif type(d[args.key]) != type([]):
-                    tv = d[args.key]
-                    d[args.key] = []
-                    d[args.key].append(tv)
-        with open('storage.data') as feed:
-            temp = json.load(feed)
-        temp.append(d)
-        with open('storage.data', 'w') as f:
-            json.dump(d, f)
+                d[args.key].append(args.val)
+            with open(os.path.join(tempfile.gettempdir(), 'storage.data'), 'w') as f:
+                json.dump(d, f)
 
 
-    if args.key and not args.val:
-        with open(storage_path) as json_file:
-            data = json.load(json_file)
-            print(data)
+        if args.key and not args.val:
+            with open(os.path.join(tempfile.gettempdir(), 'storage.data')) as json_file:
+                data = json.load(json_file)
+                if data.get(args.key) != None:
+                    print(*(data.get(args.key)), sep=', ')
+                else:
+                    print('')
 
 if __name__ == "__main__":
     my_func()
