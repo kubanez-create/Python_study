f4b973478424dba7eabaa67f4d22ea7c6c813853
kubanez-create <57756022+kubanez-create@users.noreply.github.com>
2020-09-22 17:38:53 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,8 +1,8 @@
-import pandas as pd
+import csv
 import os
 
 class CarBase:
-    def __init__(self, brand, photo_file_name, carrying):
+    def __init__(self, brand=None, photo_file_name=None, carrying=None):
         self.brand = brand
         self.photo_file_name = photo_file_name
         self.carrying = carrying
@@ -14,30 +14,39 @@ class CarBase:
 
 
 class Car(CarBase):
-    def __init__(self, brand, photo_file_name, carrying, passenger_seats_count):
+    def __init__(self, brand=None, photo_file_name=None, carrying=None, passenger_seats_count=None):
         super().__init__(brand, photo_file_name, carrying)
-        self.passenger_seats_count=passenger_seats_count
+        self.passenger_seats_count = int(passenger_seats_count)
 
 
 class Truck(CarBase):
-    def __init__(self, brand, photo_file_name, carrying, body_whl):
+    def __init__(self, brand=None, photo_file_name=None, carrying=None, body_whl=None):
         super().__init__(brand, photo_file_name, carrying)
         self.body_whl = body_whl
-        dim = [d for d in body_whl.split('x')]
+        if len(self.body_whl) == 0:
+            self._body_length = 0
+            self._body_width = 0
+            self._body_height = 0    
+        dim = [float(d) for d in self.body_whl.split('x')]
         self._body_length = dim[0]
         self._body_width = dim[1]
         self._body_height = dim[2]
 
     def get_body_volume(self):
-        return self._body_length * self._body_width * self._body_height
+        return round(self._body_length * self._body_width * self._body_height, 1)
 
 
 class SpecMachine(CarBase):
-    def __init__(self, brand, photo_file_name, carrying, extra):
+    def __init__(self, brand=None, photo_file_name=None, carrying=None, extra=None):
         super().__init__(brand, photo_file_name, carrying)
         self.extra = extra
 
 
 def get_car_list(csv_filename):
+    with open(csv_filename) as csv_fd:
+        reader = csv.reader(csv_fd, delimiter=';')
+        next(reader)  # пропускаем заголовок
+        for row in reader:
+            print(row)
     car_list = []
     return car_list
