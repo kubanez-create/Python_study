75b6c3f5206b0eea3103e735bed5e1d20aae622e
kubanez-create <57756022+kubanez-create@users.noreply.github.com>
2020-10-06 16:34:54 +0300

    Update solution.py
    only 4 errors!

--- solution.py
+++ solution.py
@@ -7,28 +7,27 @@ class File:
     
     def __init__(self, file_path):
         self.name = file_path
-        if os.path.exists(os.path.join(os.path.abspath(os.getcwd()), self.name)):
+        if os.path.exists(self.name):
             self.file_path = os.path.join(os.path.abspath(os.getcwd()), self.name)
         else:
             self.file_path = os.path.join(os.path.abspath(os.getcwd()), self.name)
-            os.makedirs(self.file_path)
-            with open(os.path.join(self.file_path, (self.name + '.txt')), 'w') as n:
+            with open((self.name), 'w') as n:
                 n.write('')
 
     def read(self):
-        with open(os.path.join(self.file_path, (self.name + '.txt')), 'r') as f:
+        with open((self.name), 'r') as f:
             row = f.readline()
         return row            
         
     def write(self, st):
-        with open(os.path.join(self.file_path, (self.name + '.txt')), 'w') as f:
+        with open((self.name), 'w') as f:
             f.write(st)
         return int(len(st))
 
     def __add__(self, obj):
         new_path = self.file_path + tempfile.gettempdir()[2:]
         new_obj = File(new_path)
-        with open(os.path.join(new_obj.file_path, (new_obj.name + '.txt')), 'w') as d:
+        with open((new_obj.name), 'w') as d:
             d.writelines([self.read(), obj.read()])
         return new_obj
 
@@ -36,7 +35,7 @@ class File:
         return self
     
     def __next__(self):
-        with open(os.path.join(self.file_path, (self.name + '.txt')), 'r') as d:
+        with open((self.name), 'r') as d:
             res = d.readlines()
 
         if self.count >= len(res):
@@ -46,5 +45,5 @@ class File:
         self.count += 1
         return result
 
-    def __str__(self):
+    def __repr__(self):
         return '{}'.format(self.file_path)
