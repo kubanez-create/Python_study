e1015faa7f5546104fc43246673d63ff445535ed
kubanez-create <57756022+kubanez-create@users.noreply.github.com>
2020-10-06 13:35:59 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,34 +7,25 @@ class File:
     def __init__(self, file_path):
         self.name = file_path
         if os.path.exists(os.path.join(os.path.abspath(os.getcwd()), self.name)):
-            print('we check first condition')
             self.file_path = os.path.join(os.path.abspath(os.getcwd()), self.name)
         else:
             self.file_path = os.path.join(os.path.abspath(os.getcwd()), self.name)
             os.makedirs(self.file_path)
             with open(os.path.join(self.file_path, (self.name + '.txt')), 'w') as n:
                 n.write('')
-            print('we executed second condition', self.file_path,
-            os.path.exists(file_path))
 
     def read(self):
         with open(os.path.join(self.file_path, (self.name + '.txt')), 'r') as f:
-            print(f.readline())            
+            row = f.readline()
+        return row            
         
-    def write(self):
-        with open(self.file_path, 'w') as f:
-            f.write()
-
-
-    def __enter__(self):
-        return
-
-
-    def __exit__(self):
-        pass
+    def write(self, st):
+        with open(os.path.join(self.file_path, (self.name + '.txt')), 'a') as f:
+            f.write(st)
 
     def __add__(self, obj):
-        pass
+        new_path = os.path.join(self.file_path, tempfile.gettempdir())
+        return File(new_path)
 
     def __iter__(self):
         return self
@@ -48,4 +39,4 @@ class File:
         return result
 
     def __str__(self):
-        return '{} <{}>'.format(self.name, self.email)
+        return '{}'.format(self.file_path)
