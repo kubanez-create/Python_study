27df9f2e11d2233f16ee8f51208a0dd091dcbb7f
kubanez-create <57756022+kubanez-create@users.noreply.github.com>
2020-10-06 12:52:55 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,19 +5,20 @@ class File:
 
     
     def __init__(self, file_path):
-        if os.path.exists(os.path.join(os.path.abspath(os.getcwd()), file_path)):
+        self.name = file_path
+        if os.path.exists(os.path.join(os.path.abspath(os.getcwd()), self.name)):
             print('we check first condition')
-            self.file_path = file_path
+            self.file_path = os.path.join(os.path.abspath(os.getcwd()), self.name)
         else:
-            self.file_path = os.path.join(os.path.abspath(os.getcwd()), file_path)
-            os.mkdir(self.file_path)
-            with open(self.file_path, 'w') as n:
+            self.file_path = os.path.join(os.path.abspath(os.getcwd()), self.name)
+            os.makedirs(self.file_path)
+            with open(os.path.join(self.file_path, (self.name + '.txt')), 'w') as n:
                 n.write('')
             print('we executed second condition', self.file_path,
             os.path.exists(file_path))
 
     def read(self):
-        with open(self.file_path, 'r') as f:
+        with open(os.path.join(self.file_path, (self.name + '.txt')), 'r') as f:
             print(f.readline())            
         
     def write(self):
